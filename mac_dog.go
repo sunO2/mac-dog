216f60856e2d90770327bb17266e1fd3eb33a094
hezhihu89@gmail.com <hezhihu89@gmail.com>
2021-03-01 16:09:18 +0800

    first commit

---
+++ mac_dog.go
@@ -0,0 +1,124 @@
+package main
+import(
+ "fmt"
+ "os"
+ "io"
+ "os/user"
+ "regexp"
+ "os/exec"
+ "strings"
+ "container/list"
+ "bufio"
+)
+
+///离线设备
+func offline(offlineMac string){
+  fmt.Println("离线设备：" + offlineMac)
+}
+
+///在线设备
+func online(onlineMac string){
+  fmt.Println("上线设备：" + onlineMac)
+}
+
+
+func listString(list []string) string{
+  var stringlist string
+  for i:=0;i<len(list);i++{
+     if i == 0{
+       stringlist = fmt.Sprintf("%s",list[i])
+     }else{
+       stringlist = fmt.Sprintf("%s,%s",stringlist,list[i])
+     }
+  }
+  return stringlist
+}
+
+func contain(maclist []string,contain string) bool {
+  for i := 0; i< len(maclist); i++{
+    if maclist[i] == contain{
+      return true
+    }
+  }
+  return false
+}
+
+func containList(list list.List,contain string) bool {
+   for e :=list.Front(); e!=nil; e=e.Next(){
+      if contain == e.Value{
+        return true
+      }
+   }
+   return false
+}
+
+func main(){
+  var err error
+  var whoami []byte
+  fmt.Println("正在分析数据...")
+  cmd:=exec.Command("/bin/sh","-c","sudo nmap -sP '192.168.0.*'")
+  if whoami,err = cmd.Output(); err != nil{
+    fmt.Println(err)
+    return
+  }
+  user,uerr:=user.Current()
+  var homeDir string
+  if nil != uerr{
+   fmt.Println(uerr)
+   return
+  }
+  homeDir = user.HomeDir
+  file,e := os.OpenFile(homeDir+"/.maclist",os.O_CREATE|os.O_RDWR,0666)
+  if e != nil {
+    fmt.Println("读取文件：文件打开失败",e)
+    return
+  }
+  defer file.Close()
+  reader := bufio.NewReader(file)
+  buf,err := reader.ReadBytes('\n')
+  if err != nil {
+    if err != io.EOF{
+     fmt.Println(" error = ",err)
+     return
+    }
+  }
+  //文件中mac 地址
+  fileMacstring := string(buf)
+  fileMacList := strings.Split(fileMacstring,",")
+  reg:=regexp.MustCompile("[A-F\\d]{2}:[A-F\\d]{2}:[A-F\\d]{2}:[A-F\\d]{2}:[A-F\\d]{2}:[A-F\\d]{2}")
+  if reg==nil {
+    fmt.Println("正则异常")
+    return
+  }
+  result:=reg.FindAllStringSubmatch(string(whoami),-1)
+  var MACList []string
+  for i:=0;i<len(result);i++{
+    deviceMac := result[i][0]
+    MACList = append(MACList,deviceMac)
+    if !contain(fileMacList,deviceMac){
+          online(deviceMac)
+    }
+  }
+   fmt.Println("")
+   fmt.Println("")
+   fmt.Println("")
+  for ii := 0; ii < len(fileMacList); ii++ {
+    if !contain(MACList,fileMacList[ii]){
+       offline(fileMacList[ii])
+    }
+  }
+
+  maclist := listString(MACList)
+  fmt.Println("列表文件已保存")
+
+   file,e = os.OpenFile(homeDir+"/.maclist",os.O_TRUNC|os.O_CREATE|os.O_RDWR,0666)
+   if e != nil {
+    fmt.Println("写入文件：文件打开失败",e)
+    return
+  }
+  defer file.Close()
+
+  write := bufio.NewWriter(file)
+  write.WriteString(maclist)
+  write.Flush()
+}
